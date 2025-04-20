15c013f406527455fbca0bd6b92f265d8f40c3a5
itWasntMe13 <michal.rakoczy@edu.uekat.pl>
2025-04-20 19:27:06 +0200

    djfladskfja
    dafasd

--- core/config.py
+++ core/config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 # Ogólne
 OPENAI_VERSION = check_openai_version() # Wersja zainstalowanej biblioteki OpenAI
-MY_GPT_API_KEY = "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" # Klucz API
 CHARACTERS_LIMIT = 10000 # Limit znaków w jednym zapytaniu do API
 TOKEN_LIMIT = 4096 # Limit tokenów w jednym zapytaniu do API
 DEFAULT_MODEL = "gpt-3.5-turbo" # Domyślny model do zapytań do API
